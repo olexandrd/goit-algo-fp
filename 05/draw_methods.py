86c7ce4927dde6d5e704d306c8ac3ca0fad8ed58
Olexandrd <olexandrd@gmail.com>
2024-01-23 22:55:57 +0200

    05 change colors method

--- 05/draw_methods.py
+++ 05/draw_methods.py
@@ -1,10 +1,12 @@
 import uuid
+from copy import copy
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 class Node:
-    def __init__(self, key, color=int("ffffff", 16)):
+    def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
         self.val = key
@@ -12,11 +14,11 @@ class Node:
         self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
 
 
-def add_edges(graph, node, pos, x=0, y=0, layer=1, color=int("ffffff", 16)):
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
         graph.add_node(
             node.id, color=node.color, label=node.val
-        )
+        )  # Використання id та збереження значення вузла
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2**layer
@@ -47,16 +49,32 @@ def draw_tree(tree_root):
     plt.show()
 
 
-def build_tree(graph:nx.DiGraph, index=0):
-    if index < len(graph.edges):
-        node = Node(graph[index])
-        node.color = (0, 0.2* index + 0.3, 0.8 * index + 0.3)
-        node.left = build_tree(graph, 2 * index + 1)
-        node.right = build_tree(graph, 2 * index + 2)
-        return node
-    return None
+def insert_to_tree(root, key, color):
+    if root is None:
+        return Node(key, color=color)
+    else:
+        if key < root.val:
+            root.left = insert_to_tree(root.left, key, color=color)
+        else:
+            root.right = insert_to_tree(root.right, key, color=color)
+    return root
 
 
-def draw_tree(graph:nx.DiGraph):
-    nodes = build_tree(graph)
-    draw_tree(nodes)
+def build_binary_tree(node_list: list, colormap: list):
+    node_dict = zip(node_list, colormap)
+    root = None
+    for node, color in node_dict:
+        root = insert_to_tree(root, node, color)
+    return root
+
+
+def drawing(nodes, colormap: list = None):
+    root = build_binary_tree(nodes, colormap)
+    draw_tree(root)
+
+
+if __name__ == "__main__":
+    data = [10, 17, 4, 2, 3, 1, 7, 6, 8, 12, 11, 13, 19, 18, 42]
+    colormap = [(1-(0.7/x), 1 - (0.5/x) ** 1.1, (0.1/x)**1.6) for x in (range(1, len(data)+1))]
+    print(colormap)
+    drawing(data, colormap)
